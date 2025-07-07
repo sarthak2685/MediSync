c4596555820abed2cd77c26fe713559026398374
sarthak <sarthaks2711btechcse2025@kccitm.edu.in>
2025-07-07 09:38:25 +0530

    done

--- src/component/Doctor/Dashboard.jsx
+++ src/component/Doctor/Dashboard.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import Sidebar from "../Doctor/DocotrSidebar";
 import Header from "../Doctor/DoctorHeader";
 import config from "../../Config";
-
 const DoctorDashboard = () => {
   const [profile, setProfile] = useState({});
   const [appointments, setAppointments] = useState([]);
