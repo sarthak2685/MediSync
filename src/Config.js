2666b89875538e64f5de18043740fbcf358b3583
Sarthak Rai <106958899+sarthak2685@users.noreply.github.com>
2025-07-07 10:37:59 +0530

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 const Config = {
-    apiUrl: "http://localhost:5000/api", 
-
+    // apiUrl: "http://localhost:5000/api", 
+    apiUrl: "https://medisync-backend-1.onrender.com",
 }
 export default Config;
